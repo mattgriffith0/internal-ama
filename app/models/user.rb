fa5f643560f3038c3a02e132b4a21113523e5c72
Matt Griffith <mattgriffith0@gmail.com>
2021-10-07 16:03:10 -0700

    add user model

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable
+
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@thinkific\.com\z/, message: "You must register using an @thinkific.com account" }
+end

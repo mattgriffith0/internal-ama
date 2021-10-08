cabbc7e64b325ff8ba44596dbce50952d89b2ee6
Matt Griffith <mattgriffith0@gmail.com>
2021-10-07 19:12:24 -0700

    add event model and pages

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,31 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+end
---
+++ app/models/event.rb
@@ -0,0 +1,7 @@
+class Event < ApplicationRecord
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :date, presence: true
+end

0a7216121b658b77d854b34ffd4c844ac019e5d5
Matt Griffith <mattgriffith0@gmail.com>
2021-10-11 11:26:37 -0700

    update users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @questions = @user.questions
   end
 
   def index
-    @users = User.all
+    @users = User.all 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :events
-  has_many :questions
+  has_many :questions, through: :events
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
